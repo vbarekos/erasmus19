692fefe02c1a5fdcd92c397acdf0f0412afd0a52
Vasilis Mparekos <vbarekos@gmail.com>
2020-01-18 17:49:00 +0200

    Add files via upload

--- EV3-ColourSorter/Team2/NewBelt.java
+++ EV3-ColourSorter/Team2/NewBelt.java
@@ -21,7 +21,7 @@ public class NewBelt {
 
 	// Create a motor object to control the Medium Motor that is used as a servo
 	private static EV3MediumRegulatedMotor servoMotor;
-	
+
 	// Colour Sensor for reading colours
 	private static EV3ColorSensor ColorSensor;
 
@@ -29,7 +29,7 @@ public class NewBelt {
 		// Create the instances
 		beltMotor = new UnregulatedMotor(MotorPort.B);
 		servoMotor = new EV3MediumRegulatedMotor(MotorPort.C);
-		
+
 		Port s2 = LocalEV3.get().getPort("S2");
 		ColorSensor = new EV3ColorSensor(s2);
 
@@ -41,16 +41,20 @@ public class NewBelt {
 		ColorSample = new float[ColorProvider.sampleSize()];
 
 		// Array of size 5 to store 5 colour readings
-		// We take 5 readings for each block to be more confident on our colour decision
+		// We take 5 readings for each block to be more confident on our colour
+		// decision
 		float[] ColorSamples = new float[5];
 		int i, angle = 0;
 
-		// Whole program keeps running until the user presses the Enter button of the EV3 Smart Brick
+		// Whole program keeps running until the user presses the Enter button
+		// of the EV3 Smart Brick
 		while (Button.ENTER.isUp()) {
 
-			// ServoMotor object assumes it is at zero angle position when created, so
+			// ServoMotor object assumes it is at zero angle position when
+			// created, so
 			// robot must be in it's starting configuration when starts.
-			// Reposition hitting lever's servoMotor to 0 degrees when it offsets by errors (Correcting big errors)
+			// Reposition hitting lever's servoMotor to 0 degrees when it
+			// offsets by errors (Correcting big errors)
 			if (angle >= 20) {
 				servoMotor.rotate(-20);
 				angle -= 20;
@@ -59,7 +63,7 @@ public class NewBelt {
 			// Start moving forward the belt at constant low power
 			driveBelt(17);
 
-			// Take 1 colour sample reading with  the Colour Sensor
+			// Take 1 colour sample reading with the Colour Sensor
 			ColorProvider.fetchSample(ColorSample, 0);
 			ColorSamples[0] = ColorSample[0];
 			Delay.msDelay(100);
@@ -68,7 +72,8 @@ public class NewBelt {
 			// brick is detected,other ie it excludes detection of values:
 			// colorID -1 by the sensor that is "nothing detected"
 			// colorID 7.0 by the sensor that is BROWN colour
-			// colorID 1.0 by the sensor that is BLACK colour (our conveyor belt colour)
+			// colorID 1.0 by the sensor that is BLACK colour (our conveyor belt
+			// colour)
 			if ((ColorSamples[0] != -1) && (ColorSamples[0] != 7.0)
 					&& (ColorSamples[0] != 1.0)) {
 
@@ -76,7 +81,8 @@ public class NewBelt {
 				// Colour codes: 2 is Blue, 6 is White, 3 Green, 4 Yellow, 5 Red
 				System.out.println(ColorSample[0]);
 
-				// Take 4 more Colour sample readings and store them in the array ColorSamples
+				// Take 4 more Colour sample readings and store them in the
+				// array ColorSamples
 				for (i = 1; i < ColorSamples.length; i++) {
 					ColorProvider.fetchSample(ColorSample, 0);
 					ColorSamples[i] = ColorSample[0];
@@ -85,21 +91,22 @@ public class NewBelt {
 				}
 
 				if (isAllEqual(ColorSamples)) {
-					// Make a sound when a coloured brick is read (5 matching reading samples)
+					// Make a sound when a coloured brick is read (5 matching
+					// reading samples)
 					Sound.beepSequence();
 
 					// demonstrate rotate to target angle without wait
 					servoMotor.resetTachoCount();
-				}
 
-				// Blue bricks hit to the left side
-				if (ColorSample[0] == 2f) {
-					hitBrick(-1);
-				}
+					// Blue bricks hit to the left side
+					if (ColorSample[0] == 2f) {
+						hitBrick(-1);
+					}
 
-				// White bricks hit to the right side
-				if (ColorSample[0] == 6f) {
-					hitBrick(1);
+					// White bricks hit to the right side
+					if (ColorSample[0] == 6f) {
+						hitBrick(1);
+					}
 				}
 
 			}
@@ -110,21 +117,26 @@ public class NewBelt {
 
 	}
 
-	// Used to drive the conveyor belt at a constant speed (feeding the Lego bricks)
+	// Used to drive the conveyor belt at a constant speed (feeding the Lego
+	// bricks)
 	private static void driveBelt(int power) {
 		beltMotor.setPower(power);
 		beltMotor.forward();
 	}
 
-	// Used to move the lever that hits and sorts the bricks (direction flag: -1 is LEFT, 1 is RIGHT) 
-	// This lever has to move very fast and with large acceleration to hit hard the bricks
+	// Used to move the lever that hits and sorts the bricks (direction flag: -1
+	// is LEFT, 1 is RIGHT)
+	// This lever has to move very fast and with large acceleration to hit hard
+	// the bricks
 	private static void hitBrick(int direction) {
-		// Sets desired motor speed, in degrees per second. The maximum reliably velocity is 100 x battery voltage
+		// Sets desired motor speed, in degrees per second. The maximum reliably
+		// velocity is 100 x battery voltage
 		servoMotor.setSpeed(1700);
-		
-		// Sets the acceleration rate of this motor in degrees/sec/sec. The default value is 6000
+
+		// Sets the acceleration rate of this motor in degrees/sec/sec. The
+		// default value is 6000
 		servoMotor.setAcceleration(36000);
-		
+
 		// BLUE bricks hit to the LEFT side
 		if (direction == -1) {
 			servoMotor.rotateTo(90, true);
@@ -143,7 +155,8 @@ public class NewBelt {
 
 	}
 
-	// Examine if all 5 readings for each block are equal, meaning that we decided with
+	// Examine if all 5 readings for each block are equal, meaning that we
+	// decided with
 	// confidence for the colour of the block read.
 	public static boolean isAllEqual(float[] a) {
 		for (int i = 1; i < a.length; i++) {
